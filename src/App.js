38c3b0233cdb4aaa72ae793b122507fdcb845154
chrisking718 <chrisking@pursuit.org>
2022-11-14 12:01:19 -0500

    facts

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Tony & Chris : Best Duo Around
         </a>
       </header>
     </div>
